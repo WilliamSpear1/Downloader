1e197a240e74a6b6354b1a98e6fb3a97304795d1
William Spearman <spearmanwm@gmail.com>
2026-06-22 09:48:02 -0400

    Fixed - Error Handling Error passed needs to be a string.

--- src/error/directory_creation_error.py
+++ src/error/directory_creation_error.py
@@ -1,5 +1,5 @@
 class DirectoryCreationError(Exception):
     def __init__(self, video_path, error):
         self.video_path = video_path
-        self.error = error
-        super().__init__(f"Directory Creation Error: {video_path}")
+        self.error_message = str(error)
+        super().__init__(f"Directory Creation Error at: {self.video_path} : {self.error_message}")
